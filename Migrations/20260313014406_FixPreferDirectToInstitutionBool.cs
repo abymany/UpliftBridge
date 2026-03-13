f201a98ac608443e45e482d9ae4e63fedd6440ca
hugehome furnishings <abhibrarllc@gmail.com>
2026-03-12 21:50:21 -0400

    Fix PreferDirectToInstitution column type to bool

---
+++ Migrations/20260313014406_FixPreferDirectToInstitutionBool.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UpliftBridge.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixPreferDirectToInstitutionBool : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
