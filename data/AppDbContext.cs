b3ec781fe904e6064430e628861348b3e5265b37
hugehome furnishings <abhibrarllc@gmail.com>
2026-03-15 18:19:13 -0400

    Remove wrong bool to int conversions from AppDbContext

--- data/AppDbContext.cs
+++ data/AppDbContext.cs
@@ -15,7 +15,6 @@ namespace UpliftBridge.Data
 
         public DbSet<Story> Stories { get; set; }
 
-        // REQUIRED for updates / thank-you posts
         public DbSet<NeedUpdate> NeedUpdates { get; set; }
 
         public DbSet<NeedPhoto> NeedPhotos => Set<NeedPhoto>();
@@ -23,31 +22,6 @@ namespace UpliftBridge.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-
-            // If your Postgres schema stores these as INTEGER (0/1),
-            // this conversion prevents:
-            // "argument of WHERE must be type boolean, not type integer"
-
-            modelBuilder.Entity<Need>()
-                .Property(x => x.IsPublished)
-                .HasConversion<int>();
-
-            modelBuilder.Entity<NeedUpdate>()
-                .Property(x => x.IsVisible)
-                .HasConversion<int>();
-
-            modelBuilder.Entity<Story>()
-                .Property(x => x.IsPublished)
-                .HasConversion<int>();
-
-            // Uncomment later only if those columns are also int(0/1) in DB:
-            // modelBuilder.Entity<Need>()
-            //     .Property(x => x.PreferDirectToInstitution)
-            //     .HasConversion<int>();
-
-            // modelBuilder.Entity<GiftOrder>()
-            //     .Property(x => x.IsAnonymous)
-            //     .HasConversion<int>();
         }
     }
 }
