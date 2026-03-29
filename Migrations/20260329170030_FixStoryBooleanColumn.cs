ef3db6bef6e22f2bcd2d31a9a9681a6b279ae139
hugehome furnishings <abhibrarllc@gmail.com>
2026-03-29 13:04:13 -0400

    Fix Stories IsPublished column

---
+++ Migrations/20260329170030_FixStoryBooleanColumn.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UpliftBridge.Migrations
+{
+    public partial class FixStoryBooleanColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            if (migrationBuilder.ActiveProvider.Contains("Npgsql"))
+            {
+                migrationBuilder.Sql(@"
+                    ALTER TABLE ""Stories""
+                    ALTER COLUMN ""IsPublished"" TYPE boolean
+                    USING CASE
+                        WHEN ""IsPublished"" = 1 THEN true
+                        ELSE false
+                    END;
+                ");
+            }
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            if (migrationBuilder.ActiveProvider.Contains("Npgsql"))
+            {
+                migrationBuilder.Sql(@"
+                    ALTER TABLE ""Stories""
+                    ALTER COLUMN ""IsPublished"" TYPE integer
+                    USING CASE
+                        WHEN ""IsPublished"" = true THEN 1
+                        ELSE 0
+                    END;
+                ");
+            }
+        }
+    }
+}
