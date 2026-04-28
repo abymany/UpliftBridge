0c32482a0ea0ee81c0da4f52e86641671ef7ba3e
hugehome furnishings <abhibrarllc@gmail.com>
2026-04-28 10:56:52 -0400

    Force DB migration on startup

--- Program.cs
+++ Program.cs
@@ -149,5 +149,9 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 app.Run();
